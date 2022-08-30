2912cae226aab269e8b94d6d0f0719fd7bfc5b58
guilherme oliveira <oliveiraguigo00@gmail.com>
2022-08-30 19:48:43 -0300

    update filter

--- postProcessing.py
+++ postProcessing.py
@@ -8,25 +8,24 @@ def cartesian2Polar(x,y):
     theta=np.arctan2(y,x)
     return (float("{:.2f}".format(rho)), float("{:.2f}".format(theta*180)))
 
-data=np.load('/home/guilherme/Documents/Autonomous-Device-for-Analysis-and-Monitoring-of-Underwater-Structures/datasPiscina/matrixSample.npy')
-
+data=np.load('datasPiscina/matrixSample.npy')
 
 rangeSonar=1500 #1.5m
 numOfpoints=len(data[0][0])
-#print(numOfpoints)
 factorCorretion=rangeSonar/numOfpoints
 
 lines=(data.shape[0]*data.shape[1]*data.shape[2])
 
 f=np.zeros((lines,6))
  
-file=open('/home/guilherme/Documents/data.txt','w+')
+file=open('data.xyzrgb','w+')
 
 aux=0
 for z in range(len(data)):
     for y in range(len(data[z])):
         for x in range(len(data[z][y])):
-            if data[z][y][x]>230 and  data[z][y][x]<250:
+            #if data[z][y][x]>235 and  data[z][y][x]<240:
+            if data[z][y][x]/255>0.98:
                 #a=np.max(data[z][y])
                 #x=np.median(a)
                 rho,theta=cartesian2Polar(int(x)*factorCorretion,int(y)*0.9)
@@ -43,11 +42,6 @@ for z in range(len(data)):
 
 file.close()
 print("Loading a point cloud")
-pcd = o3d.io.read_point_cloud('/home/guilherme/Documents/data.txt',format='xyzrgb')
+pcd = o3d.io.read_point_cloud('data.xyzrgb',format='xyzrgb')
 print(pcd)
-print(np.asarray(pcd.points))
-o3d.visualization.draw_geometries([pcd],
-                                  zoom=0.3412,
-                                  front=[0.4257, -0.2125, -0.8795],
-                                  lookat=[2.6172, 2.0475, 1.532],
-                                  up=[-0.0694, -0.9768, 0.2024])
+o3d.visualization.draw_geometries([pcd])
