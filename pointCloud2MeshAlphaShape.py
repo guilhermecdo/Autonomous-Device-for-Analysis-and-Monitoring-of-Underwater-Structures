02e3dd6c39f2bdaae9b1e8cadbf7e2c6d193f1c5
Guilherme <oliveiraguigo00@gmail.com>
2022-09-21 18:22:04 -0300

    add alpha shape mesh generation

---
+++ pointCloud2MeshAlphaShape.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import open3d as o3d
+import numpy as np
+
+print("Loading a point cloud")
+pcd = o3d.io.read_point_cloud('dataFilter.xyz',format='xyz')
+
+pcd = pcd.voxel_down_sample(voxel_size=5)
+pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+print(pcd)
+
+with o3d.utility.VerbosityContextManager(
+        o3d.utility.VerbosityLevel.Debug) as mm:
+    labels = np.array(
+        pcd.cluster_dbscan(eps=100, min_points=5, print_progress=True))
+
+alpha = 45
+print(f"alpha={alpha:.3f}")
+mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
+mesh.compute_vertex_normals()
+print(mesh)
+o3d.visualization.draw_geometries([mesh])
+
+radii = [10, 20, 40, 80]
+rec_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(
+    pcd, o3d.utility.DoubleVector(radii))
+o3d.visualization.draw_geometries([rec_mesh])
